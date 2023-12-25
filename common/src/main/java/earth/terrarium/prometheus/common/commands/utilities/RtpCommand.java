96d9615f2a276b3539c1eeadfdd6cbcb5d93c62d
Dietrich Friday <friday.dietrich@gmail.com>
2023-12-25 09:21:22 +0900

    Avoid Math.random()
    Added lines to for read-a-bility.

--- common/src/main/java/earth/terrarium/prometheus/common/commands/utilities/RtpCommand.java
+++ common/src/main/java/earth/terrarium/prometheus/common/commands/utilities/RtpCommand.java
@@ -74,11 +74,13 @@ public class RtpCommand {
         final int min = distance / 4;
         final int max = distance - min;
 
+        boolean flipX = player.getRandom().nextDouble() < 0.5;
         int randRangeX = min + player.getRandom().nextInt(max);
-        int x = location.getX() + (randRangeX * ((Math.random() < 0.5) ? -1 : 1));
+        int x = location.getX() + (randRangeX * (flipX ? -1 : 1));
 
+        boolean flipZ = player.getRandom().nextDouble() < 0.5;
         int randRangeZ = min + player.getRandom().nextInt(max);
-        int z = location.getZ() + (randRangeZ * ((Math.random() < 0.5) ? -1 : 1));
+        int z = location.getZ() + (randRangeZ * (flipZ ? -1 : 1));
 
         if (!level.getWorldBorder().isWithinBounds(x, z) || level.getBiome(new BlockPos(x, level.getSeaLevel(), z)).is(BiomeTags.IS_OCEAN)) {
             return tp(location, player, distance, tries + 1);
