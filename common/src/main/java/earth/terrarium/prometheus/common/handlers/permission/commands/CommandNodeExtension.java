442812c24832d87dbe613a85cdbfe114bde45b5b
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2024-09-04 16:34:00 -0400

    delete interface

--- common/src/main/java/earth/terrarium/prometheus/common/handlers/permission/commands/CommandNodeExtension.java
+++
@@ -1,8 +0,0 @@
-package earth.terrarium.prometheus.common.handlers.permission.commands;
-
-import com.mojang.brigadier.tree.CommandNode;
-
-public interface CommandNodeExtension {
-
-    void prometheus$removeChild(CommandNode<?> child);
-}
